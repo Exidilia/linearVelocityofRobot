645e48907d7721d507f19d22686efd8b39c234af
zyghsm <zyghsm@protonmail.com>
2020-10-08 22:03:58 +0330

    edited the comment line

--- linearVelocityofRobot.py
+++ linearVelocityofRobot.py
@@ -1,5 +1,5 @@
-###### to calculate the linear velocity of my robot ######
-############## Zahra Ghasemi - 955367022 #################
+###### to calculate the linear velocity of my robotic arm ######
+############## Zahra Ghasemi - 955367022 #######################
 
 import math
 import numpy as np
